d116c1f285c15a0e84d241f372cc5c06e16f2bf6
Shera713 <t.sheratoledo@gmail.com>
2025-10-27 14:42:16 -0400

    sort-function

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function renderMovies(searchTerm) {
     const response = await fetch(`https://omdbapi.com/?s=${searchTerm}&apikey=a32eca9f`);
     const data = await response.json()
    const moviesArr = data.Search
-   moviesWrapper.innerHTML = moviesArr.sort((a, b) => a.Year - b.Year).slice(0, 6).map((movie) => {
+   moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
 return`
  <div class="movie-card">
  <img src=${movie.Poster} alt="">
@@ -34,7 +34,29 @@ return`
  
 //}
 
+function renderMovieSort(){
+ const movieSortWrapper =document.querySelector( 'movieSort');
 
+ movieSortWrapper.innerHTML = ` <div class="movie-card">
+ <img src=${movie.Poster} alt="">
+  <h2>${movie.Title}</h2>
+  <h4>${movie.Year}</h4>
+  </div>`
+}
+
+setTimeout(() => {
+    renderMovieSort();
+});
+
+
+
+
+
+
+
+
+
+//.sort((a, b) => a.Year - b.Year)
 
 function orderListBy(event) {
   return function (a, b) {
